b33ffc65625e324202bf923fc92ac1f38bd730f8
TimothyY06 <tyoung40@stuy.edu>
2024-03-25 15:42:47 -0400

    hello

--- Practice.java
+++ Practice.java
@@ -1,1 +1,20 @@
+<dependency>
+    <groupId>commons-io</groupId>
+    <artifactId>commons-io</artifactId>
+    <version>2.15.1</version>
+</dependency>
 
+import java.io.*;
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] commandLine) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+        FileUtils.writeByteArrayToFile(output.txt, dataForWriting);
+    }
+}
